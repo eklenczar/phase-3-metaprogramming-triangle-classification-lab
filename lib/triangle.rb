bdd720e3a7656067bd511c83f16adca8287ae806
Elizabeth Klenczar <eklenczar@gmail.com>
2022-08-01 15:02:36 -0400

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+require "pry"
+
 class Triangle
-  # write code here
+
+  attr_accessor :length_1, :length_2, :length_3
+
+  def initialize(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end
+
+  def kind
+    # binding.pry
+    if self.length_1 <= 0 || self.length_2 <= 0 || self.length_3 <= 0
+      # binding.pry
+      raise TriangleError
+    elsif (length_1 + length_2) <= length_3 || (length_1 + length_3) <= length_2 || (length_3 + length_2) <= length_1
+      raise TriangleError
+    else
+      if self.length_1 == self.length_2 && self.length_2 == self.length_3
+        :equilateral
+      elsif self.length_1 == self.length_2 || self.length_1 == self.length_3 || self.length_2 == self.length_3
+        :isosceles
+      else 
+        :scalene
+      end
+    end
+    
+  end
+
+  class TriangleError < StandardError
+    
+  end
 end
